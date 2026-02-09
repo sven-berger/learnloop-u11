f3b5d755e19494e17e8ebc7053ebb9d8edd2b402
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-02-09 18:39:51 +0100

    Create header.php with HTML and Bootstrap setup
    Add header.php file with basic HTML structure and Bootstrap integration.

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>LearnLoop U11</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+      crossorigin="anonymous"
+    />
+  </head>
+
+  <body>
+    <!-- Navigation -->
+   <?php require_once("navbar.php"); ?>
+    
+    <!-- Hero -->
+   <?php require_once("hero.php"); ?>
+
+    <!-- Main -->
+    <?php require_once("content.php"); ?>
+    
+    <main class="container mt-4">
+      <div class="row">
+        <div class="col-12 col-md-8 col-lg-6 mb-5">
