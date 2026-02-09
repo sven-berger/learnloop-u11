4fea54c4966cc800f6e2626c95a5051ca09b4d1e
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-02-09 18:40:32 +0100

    Add hero.php with a greeting header

---
+++ includes/hero.php
@@ -0,0 +1,10 @@
+    <header class="bg-warning py-5">
+      <div class="container">
+        <h1 class="mb-0">
+          <?php
+            $world = "Welt";
+            echo "Hallo " . $world . "!";
+          ?>
+        </h1>
+      </div>
+    </header>
