42e5032d8310227aeec9b047f826dde426ff23ee
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-02-09 22:41:14 +0100

    Test Edit

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
     </select>
   </div>
 
-  <button type="submit" class="btn btn-danger">Absenden</button>
+  <button type="submit" class="btn btn-warning">Absenden</button>
 </form>
 <?php echo $contentEnd; ?>
 
