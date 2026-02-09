7612f6a23a8b30f755e96aba0f0286e4869abb10
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-02-09 18:41:14 +0100

    Add navbar component to includes/navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,25 @@
+ <nav class="navbar navbar-expand-md bg-light border-bottom">
+      <div class="container">
+        <a class="navbar-brand" href="#">Demo</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#menu"
+          aria-controls="menu"
+          aria-expanded="false"
+          aria-label="Menü öffnen"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="menu">
+          <div class="navbar-nav ms-auto">
+            <a class="nav-link active" aria-current="page" href="#">Active</a>
+            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </div>
+        </div>
+      </div>
+    </nav>
